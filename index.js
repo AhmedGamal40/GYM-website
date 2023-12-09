79537bb394eecb53dd0049603cad39cdde6846c9
Ahmed <ahmed.gamal.xv@gmail.com>
2023-12-09 15:18:24 +0200

    update

--- index.js
+++ index.js
@@ -6,3 +6,24 @@ burger.addEventListener('click' , ()=>{
     navbarItems.classList.toggle('h-class')
     nav.classList.toggle('v-class')
 })
+
+
+// scrool top
+let ptnScroll = document.getElementById('scroll');
+
+window.onscroll = function () {
+  // Check if scrolled past threshold
+  if (window.scrollY >= 600) {
+    ptnScroll.style.display = "block";; // Use classList for better performance
+  } else {
+    ptnScroll.style.display = "none";
+  }
+};
+
+ptnScroll.addEventListener('click', function (event) {
+  event.preventDefault(); // Prevent default click behavior
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth', // Use scrollTo with smooth behavior for animation
+  });
+});
